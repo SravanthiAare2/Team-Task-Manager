4d11bf84868bca73ff48d7bfc527eed7e21af803
Aare Sravanthi <sravanthiaare2@gmail.com>
2026-05-25 20:45:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -368,10 +368,11 @@ def logout():
     return redirect('/login')
 
 
+# ================= CREATE DATABASE =================
+with app.app_context():
+    db.create_all()
+
 # ================= RUN =================
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
